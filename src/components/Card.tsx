50d3a01a8ffbc4d0df628976ece3317c7286e61d
Al <athila2001@gmail.com>
2024-08-07 00:36:46 +0700

    +taskplaceholder

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,9 +6,6 @@ import {
   Button,
 } from "@material-tailwind/react";
 
-const placeholder =
-  "Ornare porttitor pellentesque, hac platea id, porttitor ipsum amet sed. Suspendisse ipsum ante, vel turpis dignissim, imperdiet eget phasellus ridiculus. Convallis ut eleifend, faucibus diam porttitor, eget bibendum nulla facilisis. Fermentum auctor dignissim, in lectus nunc, sed molestie pellentesque consequat. Gravida suspendisse sed, rutrum eget faucibus, quis morbi varius sit.";
-
 export function SimpleCard({ task }) {
   const { title, desc } = task;
   return (
--- src/components/CardPlaceholder.tsx
+++ src/components/CardPlaceholder.tsx
@@ -2,40 +2,24 @@ import {
   Card,
   CardBody,
   CardFooter,
-  Typography,
   Button,
+  Textarea,
+  Input,
 } from "@material-tailwind/react";
 
 export function SimpleCardPlaceholder() {
   return (
     <Card className=" mt-6 w-96 flex-wrap">
       <CardBody>
-        <Typography color="blue-gray" className="mb-2">
-          <div className="w-72">
-            <div className="relative w-full min-w-[200px] h-10">
-              <input
-                className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
-                placeholder=" "
-              />
-              <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
-                Title
-              </label>
-            </div>
-          </div>
-        </Typography>
-        <Typography>
-          <div className="w-72">
-            <div className="relative w-full min-w-[200px] h-10">
-              <input
-                className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
-                placeholder=" "
-              />
-              <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
-                Description
-              </label>
-            </div>
-          </div>
-        </Typography>
+        <div className="flex w-72 flex-col gap-6">
+          <Input
+            crossOrigin=""
+            variant="standard"
+            label="Title"
+            placeholder="Write what you gonna do!"
+          />
+          <Textarea variant="outlined" label="Describe your task!" />
+        </div>
       </CardBody>
       <CardFooter className="pt-0">
         <Button>Add Task</Button>
