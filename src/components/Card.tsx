c119d61de9389c562010678227b39039d4b707dd
Al <athila2001@gmail.com>
2024-08-07 00:27:04 +0700

    placeholder task

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,13 +10,14 @@ const placeholder =
   "Ornare porttitor pellentesque, hac platea id, porttitor ipsum amet sed. Suspendisse ipsum ante, vel turpis dignissim, imperdiet eget phasellus ridiculus. Convallis ut eleifend, faucibus diam porttitor, eget bibendum nulla facilisis. Fermentum auctor dignissim, in lectus nunc, sed molestie pellentesque consequat. Gravida suspendisse sed, rutrum eget faucibus, quis morbi varius sit.";
 
 export function SimpleCard({ task }) {
+  const { title, desc } = task;
   return (
     <Card className=" mt-6 w-96 flex-wrap">
       <CardBody>
         <Typography variant="h5" color="blue-gray" className="mb-2">
-          {task.title}
+          {title}
         </Typography>
-        <Typography>{task.desc}</Typography>
+        <Typography>{desc}</Typography>
       </CardBody>
       <CardFooter className="pt-0">
         <Button>Read More</Button>
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -2,6 +2,7 @@ import { NavbarWithMegaMenu } from "../components/Navbar";
 import { SimpleCard } from "../components/Card";
 import ButtonAddTask from "../components/ButtonAddTask";
 import { useState } from "react";
+import { SimpleCardPlaceholder } from "../components/CardPlaceholder";
 
 const tasks = [
   {
@@ -11,18 +12,28 @@ const tasks = [
 ];
 
 export default function HomePage() {
-  const [task, setTask] = useState(tasks);
+  const [task, setTask] = useState([]);
+  const [taskPlaceholder, setTaskPlaceholder] = useState(false);
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen">
       <NavbarWithMegaMenu />
       <div className="flex flex-wrap justify-center gap-4 mt-4">
+        {/* {task.length>0 ? task.map((task) => <SimpleCard task={task} />)} */}
         {task?.map((task) => <SimpleCard task={task} />)}
 
-        <div className="relative">
-          <div className="absolute bottom-0 right-0 transform translate-x-11 translate-y-1">
-            <ButtonAddTask />
+        <SimpleCardPlaceholder />
+
+        {task.length > 0 ? (
+          <div className="relative">
+            <div
+              className="absolute bottom-0 right-0 transform translate-x-11 translate-y-1"
+              onClick={() => setTaskPlaceholder(true)}
+            >
+              <ButtonAddTask />
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
     </div>
   );
